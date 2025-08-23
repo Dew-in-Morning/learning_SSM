6c8c92440b8a6221922815f82d309b5fd0555a1a
dewfire <3151716799@qq.com>
2025-08-23 20:52:15 +0800

    将itheima分支的代码替换主分支

--- src/main/java/com/lieyan/ssm/dao/IBookDAO.java
+++
@@ -1,9 +0,0 @@
-package com.lieyan.ssm.dao;
-
-import com.lieyan.ssm.pojo.BookInfo;
-
-import java.util.List;
-
-public interface IBookDAO {
-    public List<BookInfo> getBooks() throws Exception;
-}
