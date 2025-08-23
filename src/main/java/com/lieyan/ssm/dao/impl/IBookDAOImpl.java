fd78abf64b816cc5c5b1d055b62dc52c2283d82a
dewfire <3151716799@qq.com>
2025-08-23 18:21:23 +0800

    测试类报错，根据AI得知该实现方法为过时的mybatis的原生使用方法，而我原本学习的方法为结合了Spring的现代工程处理方法。故选择弃用该课程，改为使用黑马的课程

--- src/main/java/com/lieyan/ssm/dao/impl/IBookDAOImpl.java
+++ src/main/java/com/lieyan/ssm/dao/impl/IBookDAOImpl.java
@@ -14,9 +14,9 @@ public class IBookDAOImpl implements IBookDAO {
     @Override
     public List<BookInfo> getBooks() throws Exception {
         InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
-        //工厂构建器
+        // 工厂构建器
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        //构建工厂
+        // 构建工厂
         SqlSessionFactory factory = builder.build(is);
         SqlSession session = factory.openSession();
         List<BookInfo> list = session.selectList("getBooks");
