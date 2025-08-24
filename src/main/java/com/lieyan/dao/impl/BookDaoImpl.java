ef69e654f8b85ef43b692eb9ceb373cf1cb44a8a
dewfire <3151716799@qq.com>
2025-08-24 12:17:35 +0800

    1.bean运用反射暴力获取权限来通过构造方法创建对象，private的构造方法也能访问，且只能访问无参构造；2.bean可以起别名“name=‘’”；3.bean有作用范围，可通过scope=“”配置，默认为单例，通常单例最佳，只在带参封装实体的域对象适合改为多例

--- src/main/java/com/lieyan/dao/impl/BookDaoImpl.java
+++ src/main/java/com/lieyan/dao/impl/BookDaoImpl.java
@@ -3,6 +3,9 @@ package com.lieyan.dao.impl;
 import com.lieyan.dao.BookDao;
 
 public class BookDaoImpl implements BookDao {
+    private BookDaoImpl(){
+        System.out.println("bookconstructor is running...");
+    }
     @Override
     public void save() {
         System.out.println("Dao save...");
